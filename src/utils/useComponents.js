0dfcf88be4023f968a6559275f9e6fb5d9e43ea6
Michael Braun <michael@web4brauns.de>
2020-05-18 23:51:03 +0200

    Add context-menu filtering

--- src/utils/useComponents.js
+++ src/utils/useComponents.js
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { compatibleSocketInputComparator, compatibleSocketOutputComparator } from './socket-comparator';
 
 function calcHash(components) {
     let retVal = '';
@@ -10,12 +11,47 @@ function calcHash(components) {
     return retVal;
 }
 
-export default function useComponents(editor) {
+function calcContextHash(context) {
+    if (!context) return null;
+
+    return `${context.input ? 'true' : 'false'}_${context.output ? 'true' : 'false'}_${context.socket.name}`;
+}
+
+export default function useComponents(editor, context) {
     const hash = calcHash(editor.components);
+    const contextHash = calcContextHash(context);
 
     return useMemo(() => {
-        const array = Array.from(editor.components.values());
+        let array = Array.from(editor.components.values());
+
+        const socketInputComparator = compatibleSocketInputComparator(context.socket);
+        const socketOutputComparator = compatibleSocketOutputComparator(context.socket);
+
+        if (context?.output) {
+            array = array.filter(c => {
+                if (!c.componentDefinition?.inputs) {
+                    return false;
+                }
+
+                const foundInput = c.componentDefinition.inputs.find(socketInputComparator);
+
+                return !!foundInput;
+            });
+        }
+
+        if (context?.input) {
+            array = array.filter(c => {
+                if (!c.componentDefinition?.outputs) {
+                    return false;
+                }
+
+                const foundInput = c.componentDefinition.outputs.find(socketOutputComparator);
+
+                return !!foundInput;
+            });
+        }
+
         array.sort((a, b) => a.name.localeCompare(b.name));
         return array;
-    }, [hash]); /* eslint-disable-line react-hooks/exhaustive-deps */
+    }, [hash, contextHash]); /* eslint-disable-line react-hooks/exhaustive-deps */
 }
