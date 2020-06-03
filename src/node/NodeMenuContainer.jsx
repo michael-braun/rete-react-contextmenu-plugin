ed984191c5f38c16a1d8fbbda9af70826eb5396b
Michael Braun <michael@web4brauns.de>
2020-06-03 23:51:30 +0200

    Add support for duplication

--- src/node/NodeMenuContainer.jsx
+++ src/node/NodeMenuContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
+import { deepCopy } from '../context/ContextMenuContainer';
 
 const NodeMenuContainer = ({ component: Component, node, editor, ...props }) => {
     const handleDelete = useCallback(() => {
@@ -7,11 +8,74 @@ const NodeMenuContainer = ({ component: Component, node, editor, ...props }) => 
             editor.removeNode(n);
         });
         editor.trigger('hidecontextmenu');
-    }, [editor, node]);
+    }, [editor]);
+
+    const handleClone = useCallback(async () => {
+        const promises = [];
+
+        const mappedNodes = {};
+
+        editor.selected.each((n) => {
+            promises.push((async () => {
+                const component = editor.getComponent(n.name);
+                const createdNode = await component.createNode(deepCopy(n.data));
+
+                createdNode.meta = deepCopy(n.meta);
+                createdNode.position[0] = n.position[0] + 25;
+                createdNode.position[1] = n.position[1] + 25;
+
+                editor.addNode(createdNode);
+
+                mappedNodes[n.id] = createdNode;
+
+                return {
+                    originalNode: n,
+                    createdNode,
+                };
+            })());
+        });
+
+        const createdNodes = await Promise.all(promises);
+
+        createdNodes.forEach(({ originalNode }) => {
+            originalNode.inputs.forEach((input) => {
+                input.connections.forEach((connection) => {
+                    const newInputNode = mappedNodes[connection.input.node.id];
+                    const newOutputNode = mappedNodes[connection.output.node.id];
+
+                    if (!newInputNode || !newOutputNode) {
+                        return;
+                    }
+
+                    const connectionInput = newInputNode.inputs.get(connection.input.key);
+                    const connectionOutput = newOutputNode.outputs.get(connection.output.key);
+
+                    if (!connectionInput || !connectionOutput) {
+                        return false;
+                    }
+
+                    editor.connect(connectionOutput, connectionInput);
+                });
+            });
+
+            editor.selected.remove(originalNode);
+            originalNode.update();
+        });
+
+        const nodesToUpdate = [];
+        createdNodes.forEach(({ createdNode }) => {
+            editor.selected.list.push(createdNode);
+            nodesToUpdate.push(createdNode);
+        });
+        nodesToUpdate.forEach(n => n.update());
+
+        editor.trigger('hidecontextmenu');
+    }, [editor]);
 
     return (
         <Component
             onDelete={handleDelete}
+            onClone={handleClone}
             editor={editor}
             node={node}
             {...props}
